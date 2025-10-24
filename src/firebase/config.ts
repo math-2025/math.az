ae229bf683fa0570932a4746d3d024e241ca39e4
math-2025 <physicswithme.2026@gmail.com>
2025-10-24 11:42:41 +0400

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,27 +1,8 @@
-"use client";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-// This is now configured to read from Environment Variables
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+export const firebaseConfig = {
+  "projectId": "riyaziyyat-307ba",
+  "appId": "1:637843587573:web:7531c74b12753fde4a6948",
+  "storageBucket": "riyaziyyat-307ba.appspot.com",
+  "apiKey": "AIzaSyAz_-_Y1h82a7Fz8jV3TzX8tY-ZqC4_d7o",
+  "authDomain": "riyaziyyat-307ba.firebaseapp.com",
+  "messagingSenderId": "637843587573"
 };
-
-
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-
-export { app, db, storage };
